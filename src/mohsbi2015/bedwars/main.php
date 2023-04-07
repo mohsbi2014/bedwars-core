4c0d5459962d4cb358c7d5117cd714f4d36d6182
mohsbi2015 <aboshxm2@gmail.com>
2023-04-07 11:43:49 +0300

    first commit

---
+++ src/mohsbi2015/bedwars/main.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace mohsbi2015\bedwars;
+
+use pocketmine\plugin\PluginBase;
+
+class main extends PluginBase {
+  public function onEnable(): void {
+    $this->getServer()->getLogger()->info("Enabling bedwars-core made by mohsbi2015 .");
+  }
+}
